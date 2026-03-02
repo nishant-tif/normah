0e1b988dbc799b0a682c43f5b1354b37ce7d136e
Rashid Ali <rashid@tiftechnologies.com>
2026-03-03 01:12:06 +0530

    husky

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
 import type { NextConfig } from "next";
 
+
+
 const nextConfig: NextConfig = {
+
   /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+
   reactCompiler: true,
+
 };
+
+
 
 export default nextConfig;
